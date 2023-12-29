e31622b25db60dcc8f89a307b897a7ddb3f1917e
Jannatul Ferdous <137715921+jannatul-f@users.noreply.github.com>
2023-12-29 22:36:52 +0600

    Add files via upload

---
+++ programe13.py
@@ -0,0 +1,18 @@
+
+result1= 30 > 20
+print(result1)
+
+result2= 30 < 20
+print(result2)
+
+result3= 30==30
+print(result3)
+
+print(30 >= 20)
+print (30 <= 20)
+print(30!=20)
+print("dola"=="hamim")
+
+
+
+
